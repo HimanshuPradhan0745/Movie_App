6b61485fcccf428bbf8c692dee4113967adea714
HimanshuPradhan0745 <pradhanhimanshu206@gmail.com>
2022-03-19 01:00:18 +0530

    first commit

---
+++ my_movie_app/src/App.js
@@ -0,0 +1,163 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import React, { useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import MovieComponent from "./components/MovieComponent";
+import MovieInfoComponent from "./components/MovieInfoComponent";
+
+export const API_KEY = "a9118a3a";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: bisque
+
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 25px;
+  margin-left: 20px;
+  width: 30%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const MovieImage = styled.img`
+  width: 68px;
+  height: 58px;
+  margin: 15px;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;;
+
+`;
+const Placeholder = styled.img`
+  width: 320px;
+  height: 320px;
+  margin: 70px;
+  opacity: 100%;
+  border-radius:155px
+
+`;
+
+
+function App() {
+  const [searchQuery, updateSearchQuery] = useState("");
+
+  const [movieList, updateMovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
+
+  const [timeoutId, updateTimeoutId] = useState();
+
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`,
+    );
+    updateMovieList(response.data.Search);
+  };
+
+  const onTextChange = (e) => {
+    onMovieSelect("")
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+  return (
+    <Container>
+      <Header>
+        <AppName>
+          <MovieImage src="/movie_logo-removebg-preview.png" />
+           MOVIE MANIA
+        </AppName>
+        <SearchBox>
+          <SearchIcon src="/search logo.jpg.png" />
+          <SearchInput
+            placeholder="Search Movie"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
+      <MovieListContainer>
+        {movieList?.length ? (
+          movieList.map((movie, index) => (
+            <MovieComponent
+              key={index}
+              movie={movie}
+              onMovieSelect={onMovieSelect}
+            />
+          ))
+        ) : (
+          <Placeholder src="/Movie_mania.jpg" />
+        )}
+      </MovieListContainer>
+    </Container>
+  );
+}
+
+export default App;
